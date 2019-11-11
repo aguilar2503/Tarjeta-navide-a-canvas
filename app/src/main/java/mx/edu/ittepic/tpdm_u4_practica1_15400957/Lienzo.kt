244bcd3ec5f205c2e5e193911d0c26ffea82f33f
aguilar2503 <aguilar_.roberto@hotmail.com>
2019-11-10 20:12:33 -0700

    solucion de error en comentarios

--- app/src/main/java/mx/edu/ittepic/tpdm_u4_practica1_15400957/Lienzo.kt
+++ app/src/main/java/mx/edu/ittepic/tpdm_u4_practica1_15400957/Lienzo.kt
@@ -13,13 +13,13 @@ class Lienzo(p:MainActivity): View(p){
 
         setBackgroundColor(Color.rgb(63, 65, 75))
 
-        //montañas
+
         p.color=Color.WHITE
         c?.drawCircle(200f,500f,1000f,p)
         p.color=Color.WHITE
         c?.drawCircle(100f,2100f,1000f,p)
 
-        //ESTRELLAS
+
         p.color=Color.rgb(113,202,241)
         c?.drawCircle(1300f,500f,10f,p)
 
@@ -37,7 +37,7 @@ class Lienzo(p:MainActivity): View(p){
 
         p.color=Color.rgb(113,202,241)
         c?.drawCircle(1300f,2400f,10f,p)
-        //-----------------arbol 1--------------------------------
+
         p.color=Color.rgb(120,0,0)
         c?.drawRect(150f,1260f, 400f,1315f,p )
         //izquierda, arriba,    derecha,  abajo
@@ -48,17 +48,16 @@ class Lienzo(p:MainActivity): View(p){
         p.color=Color.GREEN
         c?.drawCircle(400f,1310f,40f,p)
 
-        //-----------------Arbol2------------------------------
+
         p.color=Color.rgb(120,0,0)
         c?.drawRect(650f,750f, 800f,810f,p )
-        //izquierda, arriba,    derecha,  abajo
+
         p.color=Color.GREEN
         c?.drawCircle(820f,740f,40f,p)
         p.color=Color.GREEN
         c?.drawCircle(870f,770f,40f,p)
         p.color=Color.GREEN
         c?.drawCircle(820f,810f,40f, p)
-        //----------arbol3-----------------------------------
         p.color=Color.rgb(120,0,0)
         c?.drawRect(1000f,2250f, 1100f,2300f,p )
         p.color=Color.GREEN
@@ -68,22 +67,20 @@ class Lienzo(p:MainActivity): View(p){
         p.color=Color.GREEN
         c?.drawCircle(1100f,2300f,40f,p)
 
-        //-------------casa1----------------------------
+
 
         p.color=Color.BLUE
         c?.drawRect(150f,200f, 400f,500f,p )
-        //izquierda, arriba,    derecha,  abajo
-        //chimenea
+
         p.color=Color.rgb(123,110,128)
         c?.drawRect(400f,450f, 480f,500f,p )
-        //PUERTA
+
         p.color=Color.rgb(145,23,4)
         c?.drawRect(150f,250f, 260f,320f,p )
-        //izquierda, arriba,    derecha,  abajo
-        //VENTANA
+
         p.color=Color.YELLOW
         c?.drawRect(290f,390f, 360f,460f,p )
-        //izquierda, arriba,    derecha,  abajo
+       
 
     }
 }
